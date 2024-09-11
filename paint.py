c2e6ca368aab3847dbc388501a55a7a46fc16924
DavidDiaaz <A01666448@tec.mx>
2024-09-11 19:16:29 +0000

    COLOR NUEVO Y DIBUJAR CIRCULO IMPLEMENTADO

--- paint.py
+++ paint.py
@@ -37,10 +37,18 @@ def square(start, end):
 
 
 def circle(start, end):
-    """Draw circle from start to end."""
-    pass  # TODO
+    "Draw circle from start to end."
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
 
+    radius = ((end.x - start.x)**2 + (end.y - start.y)**2)**0.5 / 2
+    goto(start.x + radius, start.y)
+    turtle_circle(radius)
 
+    end_fill()
+    
 def rectangle(start, end):
     """Draw rectangle from start to end."""
     pass  # TODO
@@ -79,6 +87,8 @@ onkey(lambda: color('white'), 'W')
 onkey(lambda: color('green'), 'G')
 onkey(lambda: color('blue'), 'B')
 onkey(lambda: color('red'), 'R')
+onkey(lambda: color ('purple'),'P')
+
 onkey(lambda: store('shape', line), 'l')
 onkey(lambda: store('shape', square), 's')
 onkey(lambda: store('shape', circle), 'c')
